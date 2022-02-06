80538cee56145f9012b1d3612aea66451607df29
James Kelley <james.a.kelley.mars@gmail.com>
2022-02-06 14:48:22 -0700

    pass test

--- src/Board.mjs
+++ src/Board.mjs
@@ -13,8 +13,16 @@ export class Board {
     return this.rows.map((row) => `${row.join(``)}\n`).join("");
   }
   drop(block) {
+    // the test for this doesn't test what's RETURNED from this method - it tests the board state via toString
     let centerIndex = Math.floor(this.width / 2);
     let { color } = block;
     this.rows[0][centerIndex] = color;
+  }
+  tick() {
+    // represents one unit of time passing
+    // the test for this uses the toString method, too
+    let newRow = Array(this.width).fill(`.`);
+    let topRows = this.rows.filter((row, ind) => ind !== this.height - 1);
+    this.rows = [newRow, ...topRows];
   }
 }
