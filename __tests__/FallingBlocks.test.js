b2ec8864a26de2eeb6bec471d66e017fac22a8a4
James Kelley <james.a.kelley.mars@gmail.com>
2022-02-06 14:57:35 -0700

    pass drop tests, add dirtyFlag, toggleDirtyFlag method to demonstrate use of dirty flag

--- __tests__/FallingBlocks.test.js
+++ __tests__/FallingBlocks.test.js
@@ -38,7 +38,7 @@ describe("Falling blocks", () => {
       );
     });
 
-    xit("at most one block may be falling at a time", () => {
+    it("at most one block may be falling at a time", () => {
       const before = board.toString();
       expect(() => board.drop(new Block("Y"))).toThrow("already falling");
       const after = board.toString();
--- src/Board.mjs
+++ src/Board.mjs
@@ -7,6 +7,10 @@ export class Board {
     this.width = width;
     this.height = height;
     this.rows = makeMatrixTable(width, height);
+    this.dirtyFlag = false;
+  }
+  toggleDirtyFlag() {
+    this.dirtyFlag = !this.dirtyFlag;
   }
 
   toString() {
@@ -14,9 +18,16 @@ export class Board {
   }
   drop(block) {
     // the test for this doesn't test what's RETURNED from this method - it tests the board state via toString
-    let centerIndex = Math.floor(this.width / 2);
-    let { color } = block;
-    this.rows[0][centerIndex] = color;
+    // should only allow a block to drop if the dirtyFlag is set to 'false'
+
+    if (!this.dirtyFlag) {
+      let centerIndex = Math.floor(this.width / 2);
+      let { color } = block;
+      this.rows[0][centerIndex] = color;
+      this.toggleDirtyFlag();
+    } else {
+      throw `already falling`;
+    }
   }
   tick() {
     // represents one unit of time passing
@@ -24,5 +35,6 @@ export class Board {
     let newRow = Array(this.width).fill(`.`);
     let topRows = this.rows.filter((row, ind) => ind !== this.height - 1);
     this.rows = [newRow, ...topRows];
+    this.toggleDirtyFlag();
   }
 }
